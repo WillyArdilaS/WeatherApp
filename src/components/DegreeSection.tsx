28deaacb712b6af30e2ce2f5dfcc34d892e55cd1
WillyArdilaS <williamardilasanchez@gmail.com>
2022-09-22 00:15:28 -0500

    Creation of interface and components

---
+++ src/components/DegreeSection.tsx
@@ -0,0 +1,17 @@
+import { WeatherData } from "../interfaces/WeatherData"
+
+export const DegreeSection = ({data: {temperature, icon, description}} : {data:WeatherData}) => {
+  return (
+    <>
+        <section className="text-white text-5xl font-bold">
+          <span className="text-yellow-500" id="temperature"> {temperature} </span> °C
+        </section>
+
+        <section>
+          <img src={icon} alt="Icono del clima" id="iconImg" />
+        </section>
+
+        <section className="mb-6 text-white text-xl font-bold uppercase" id="description"> {description} </section>
+    </>
+  )
+}
